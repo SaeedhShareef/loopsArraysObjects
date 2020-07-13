e389baf9e5882d467129f57430dd2a69dc3b106a
Saeed Shareef <saeedhshareef@gmail.com>
2020-07-13 19:37:34 -0400

    loopsArraysObjects

---
+++ js/app.js
@@ -0,0 +1,29 @@
+console.log("I hate the Cheesecake Factory")
+
+var kitty = {
+            "fur": "white",
+            "eyes":[1,2],
+            "legs": 4
+                }
+
+                console.log(kitty.eyes)
+
+                var dolphin = {
+                    "name" : ["n","a","m","e"],
+                    "color": "gray",
+                    "tails": 1
+                            }
+                    console.log(dolphin.name)
+                            
+                var wolf ={
+                            "fur": [0,0,0,0,0,0],
+                            "claws": "sharp",
+                            "teeth": 32
+                                    }
+                            console.log(wolf.fur)
+                        
+                var otherFunction = [1,2,3] ; 
+                for( i = 0; i < otherFunction.length; i++) {
+                    console.log(otherFunction[i]);
+                }
+                     
